da4f730c0f04150104b008e7278050d6c21e983b
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-16 10:39:36 -0300

    calculadora simples adicionada

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a operação que deseja fazer \nSoma, Subtracao, Multiplicacao, Divisao");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o 1º  número");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º número");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float resultado = 0;
+
+            switch(operacao){
+
+                case "Soma":
+                    resultado = num1+num2;
+                    break;
+
+                case "Subtracao":
+                    resultado = num1-num2;
+                    break;
+
+                case "Multiplicacao":
+                    resultado = num1*num2;
+                    break;
+
+                case "Divisao":
+                    resultado = num1/num2;
+                    break;
+
+                default:
+                    Console.WriteLine("Operação inválida");
+                    break;            
+            }
+
+            Console.WriteLine($"Calculo : {num1} e {num2} = Resultado : {resultado}");
+
+        }//fim do main
+    }
+}
